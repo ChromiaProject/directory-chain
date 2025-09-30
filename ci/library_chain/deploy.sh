20bd0329504d2b197cb2414a2adb6dc49676890c
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-09-30 15:07:37 +0200

    Fix terminology

--- ci/library_chain/deploy.sh
+++ ci/library_chain/deploy.sh
@@ -5,15 +5,15 @@ LIBRARY_NAME=$1
 EXISTING=`curl -sS "$LIBRARY_CHAIN_API_URL/query/$LIBRARY_CHAIN_BRID?type=library_chain_versioning.get_latest_library_version&lib_id=com.chromia.$LIBRARY_NAME"`
 DESCRIPTION=`echo $EXISTING | jq -r '.version_description'`
 
-BRID_EXISTING=`echo $EXISTING | jq -r '.rid'`
-BRID_NEW=`xmllint --xpath 'string(/dict/entry[@key="rid"]/bytea)' build/$LIBRARY_NAME.xml`
+RID_EXISTING=`echo $EXISTING | jq -r '.rid'`
+RID_NEW=`xmllint --xpath 'string(/dict/entry[@key="rid"]/bytea)' build/$LIBRARY_NAME.xml`
 
-if [[ "$BRID_EXISTING" == "$BRID_NEW" ]]; then
-   echo "Skipping deployment of $LIBRARY_NAME, because the BRID has not changed: $BRID_EXISTING."
+if [[ "$RID_EXISTING" == "$RID_NEW" ]]; then
+   echo "Skipping deployment of $LIBRARY_NAME, because the RID has not changed: $RID_EXISTING."
 else
    echo "Deploying $LIBRARY_NAME..."
-   echo "previous BRID: $BRID_EXISTING"
-   echo "new BRID: $BRID_NEW"
+   echo "previous RID: $RID_EXISTING"
+   echo "new RID: $RID_NEW"
 
    chr library deploy \
       --library $LIBRARY_NAME \
