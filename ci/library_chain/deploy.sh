697808b13b606c6c455cc1b4f6c515ce81772d51
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-10-20 18:12:20 +0200

    Update library chain deployment script

--- ci/library_chain/deploy.sh
+++ ci/library_chain/deploy.sh
@@ -2,24 +2,52 @@
 
 LIBRARY_NAME=$1
 
-EXISTING=`curl -sS "$LIBRARY_CHAIN_API_URL/query/$LIBRARY_CHAIN_BRID?type=library_chain_versioning.get_latest_library_version&lib_id=com.chromia.$LIBRARY_NAME"`
-DESCRIPTION=`echo $EXISTING | jq -r '.version_description'`
-
-RID_EXISTING=`echo $EXISTING | jq -r '.rid'`
 RID_NEW=`xmllint --xpath 'string(/dict/entry[@key="rid"]/bytea)' build/$LIBRARY_NAME.xml`
 
-if [[ "$RID_EXISTING" == "$RID_NEW" ]]; then
-   echo "Skipping deployment of $LIBRARY_NAME, because the RID has not changed: $RID_EXISTING."
-else
-   echo "Deploying $LIBRARY_NAME..."
-   echo "previous RID: $RID_EXISTING"
-   echo "new RID: $RID_NEW"
+EXISTING=`curl -sS "$LIBRARY_CHAIN_API_URL/query/$LIBRARY_CHAIN_BRID?type=library_chain_versioning.get_latest_library_version&lib_id=com.chromia.$LIBRARY_NAME"`
 
-   chr library deploy \
-      --library $LIBRARY_NAME \
-      --id com.chromia.$LIBRARY_NAME \
-      --version $CI_COMMIT_TAG \
-      --brid $LIBRARY_CHAIN_BRID \
-      --description "$DESCRIPTION" \
-      --url $LIBRARY_CHAIN_API_URL
+RID_EXISTING=`echo $EXISTING | jq -r '.rid'`
+if [ "$RID_EXISTING" = "null" ]; then
+  echo "First deploy of library $LIBRARY_NAME"
+  VERSION=$2
+  DESCRIPTION=$3
+
+  echo "Creating $LIBRARY_NAME $VERSION..."
+  echo "RID: $RID_NEW"
+
+  chr library create \
+    --url "$LIBRARY_CHAIN_API_URL" \
+    --brid "$LIBRARY_CHAIN_BRID" \
+    --library "$LIBRARY_NAME" \
+    --organization com.chromia \
+    --name "$LIBRARY_NAME" \
+    --version "$VERSION" \
+    --description "$DESCRIPTION"
+else
+  VERSION=${2:-$CI_COMMIT_TAG}
+  DESCRIPTION=`echo $EXISTING | jq -r '.version_description'`
+
+  if [ "$RID_EXISTING" = "$RID_NEW" ]; then
+     echo "Skipping deployment of $LIBRARY_NAME, because the RID has not changed: $RID_EXISTING."
+     exit 0
+  fi
+
+  VERSION_EXISTING=`echo $EXISTING | jq -r '.version'`
+  if [ "$VERSION_EXISTING" = "$VERSION" ]; then
+     echo "Skipping deployment of $LIBRARY_NAME, because the version has not changed: $VERSION."
+     exit 0
+  fi
+
+  echo "previous RID: $RID_EXISTING"
+
+  echo "Deploying $LIBRARY_NAME $VERSION..."
+  echo "new RID: $RID_NEW"
+
+  chr library deploy \
+    --url "$LIBRARY_CHAIN_API_URL" \
+    --brid "$LIBRARY_CHAIN_BRID" \
+    --library "$LIBRARY_NAME" \
+    --id "com.chromia.$LIBRARY_NAME" \
+    --version "$VERSION" \
+    --description "$DESCRIPTION"
 fi
